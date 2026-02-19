c7d7d3fa9fedb9c38e5682ac15047c2d32f05c41
1Code-JS <114492072+1Code-JS@users.noreply.github.com>
2026-02-19 20:53:39 +0200

    feat: Plugin ESM support

--- src/lib/loadPlugin.js
+++ src/lib/loadPlugin.js
@@ -12,64 +12,63 @@ export default async function loadPlugin(pluginId, justInstalled = false) {
 		Url.join(PLUGIN_DIR, pluginId, "plugin.json"),
 	).readFile("json");
 
-	let mainUrl;
-	if (
-		await fsOperation(Url.join(PLUGIN_DIR, pluginId, pluginJson.main)).exists()
-	) {
-		mainUrl = Url.join(baseUrl, pluginJson.main);
-	} else {
-		mainUrl = Url.join(baseUrl, "main.js");
+	let mainUrl = pluginJson.main;
+	if (!(await fsOperation(Url.join(PLUGIN_DIR, pluginId, mainUrl)).exists())) {
+		mainUrl = `main.js`;
+	}
+	mainUrl = Url.join(baseUrl, mainUrl);
+
+	const isESM = pluginJson.type === "module";
+	const $script = isESM ? null : <script src={mainUrl}></script>;
+	try {
+		const { promise, resolve, reject } = Promise.withResolvers();
+		if (isESM) {
+			const urlObj = new URL(mainUrl);
+			urlObj.searchParams.set("acodeTs", performance.now());
+			resolve(import(urlObj));
+		} else {
+			$script.addEventListener("error", () => reject());
+			$script.addEventListener("load", resolve);
+			document.head.append($script);
+		}
+		try {
+			await promise;
+		} catch (error) {
+			let name, stack, msg;
+			if (error) ({ name, stack, message: msg } = error);
+			msg =
+				`Failed to load ${isESM ? "module " : ""}` +
+				`script for plugin "${pluginId}"` +
+				(msg ? `: ${msg}` : ".");
+			error = new Error(msg, { cause: error });
+			error.name = name;
+			error.stack = stack;
+			throw error;
+		}
+	} finally {
+		$script?.remove();
 	}
 
-	return new Promise((resolve, reject) => {
-		const $script = (
-			<script id={`${pluginId}-mainScript`} src={mainUrl}></script>
-		);
+	const $page = Page("Plugin");
+	$page.show = () => {
+		actionStack.push({
+			id: pluginId,
+			action: $page.hide,
+		});
 
-		$script.onerror = (error) => {
-			reject(
-				new Error(
-					`Failed to load script for plugin ${pluginId}: ${error.message || error}`,
-				),
-			);
-		};
+		app.append($page);
+	};
 
-		$script.onload = async () => {
-			const $page = Page("Plugin");
-			$page.show = () => {
-				actionStack.push({
-					id: pluginId,
-					action: $page.hide,
-				});
+	$page.onhide = () => actionStack.remove(pluginId);
 
-				app.append($page);
-			};
+	if (!(await fsOperation(cacheFile).exists())) {
+		await fsOperation(CACHE_STORAGE).createFile(pluginId);
+	}
 
-			$page.onhide = function () {
-				actionStack.remove(pluginId);
-			};
-
-			try {
-				if (!(await fsOperation(cacheFile).exists())) {
-					await fsOperation(CACHE_STORAGE).createFile(pluginId);
-				}
-
-				await acode.initPlugin(pluginId, baseUrl, $page, {
-					cacheFileUrl: await helpers.toInternalUri(cacheFile),
-					cacheFile: fsOperation(cacheFile),
-					firstInit: justInstalled,
-					ctx: await PluginContext.generate(
-						pluginId,
-						JSON.stringify(pluginJson),
-					),
-				});
-
-				resolve();
-			} catch (error) {
-				reject(error);
-			}
-		};
-
-		document.head.append($script);
+	await acode.initPlugin(pluginId, baseUrl, $page, {
+		cacheFileUrl: await helpers.toInternalUri(cacheFile),
+		cacheFile: fsOperation(cacheFile),
+		firstInit: justInstalled,
+		ctx: await PluginContext.generate(pluginId, JSON.stringify(pluginJson)),
 	});
 }
